e6ed11596572ba50a8bac4a25b056893678f3f83
Ha3tA9 <aion9@foxmail.com>
2022-10-19 22:04:25 +0800

    OJ_2

---
+++ OJ2_10_19/OJ_2_1/OJ_2_1/OJ_2_1.c
@@ -0,0 +1,79 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+//main()
+//{
+//	printf("hello wangdao\n");
+//	return 0;
+//}
+//
+//main()
+//{
+//	int a;
+//	int b;
+//	scanf("%d%d", &a, &b);
+//	printf("a + b = %d\n", a + b);
+//	return 0;
+//}
+//
+//#include<stdio.h> //Wrong Answer
+//main()
+//{
+//	int a;
+//	int b;
+//	int n;
+//	scanf("\d \d", &a, &b);
+//	n = a + b;
+//	printf("a + b =\n", n);
+//	return 0;
+//}
+//
+//#include<stdio.h>
+//int main()
+//{
+//	int a, b;
+//	while (scanf("%d%d", &a, &b) != EOF)
+//	{
+//		printf("%d\n", a + b);
+//	}
+//	return 0;
+//}
+
+//main()
+//{
+//    int a;
+//    scanf("%d", &a);
+//    if (a < 65 && a > 122);
+//    {
+//        return 0;
+//    }
+//    else
+//    {
+//        printf("%c", a);
+//    }
+//    return 0;
+//}
+
+main()
+{
+    int a;
+    scanf("%d", &a);
+    if (a < 65 && a > 122)
+    {
+        return 0;
+    }
+    else
+    {
+        printf("%c", a);
+    }
+    return 0;
+}
+
+
+//int main()
+//{
+//	int a;
+//	scanf("%d", &a);
+//	if (65 < a < 122)
+//		printf("%c", a);
+//	return 0;
+//}
